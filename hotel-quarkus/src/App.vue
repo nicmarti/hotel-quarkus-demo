15dfe08e8044da327903efa6c3cf10fb5257b1c6
Nicolas Martignole <nicolas@martignole.net>
2019-06-05 17:35:14 +0200

    Small CSS and IMG fixes

---
+++ hotel-quarkus/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="app">
+
+        <div class="pure-menu pure-menu-horizontal">
+            <a href="#" class="pure-menu-heading">Quarkus Hotel</a>
+            <ul class="pure-menu-list">
+                <li class="pure-menu-item"><a href="/" class="pure-menu-link">Home</a></li>
+                <li class="pure-menu-item pure-menu-selected"><a href="#" class="pure-menu-link">Hotels</a></li>
+                <li class="pure-menu-item"><a href="/" class="pure-menu-link">Booking</a></li>
+            </ul>
+        </div>
+
+        <div class="banner">
+            <h1 class="banner-head">
+                Quarkus demo<br>
+                <small>Java + Vue.JS backend</small>
+            </h1>
+        </div>
+
+        <router-view :hotels="theHotels" class="hotels"></router-view>
+
+        <div class="footer l-box">
+            <p>
+                <img src="logo_quarkus.png" alt="Logo Quarkus" class="pure-image" style="width: 64px"><br>
+                <a href="https://quarkus.io/" title="Quarkus web site">Build with Quarkus</a> by Nicolas Martignole - Lunatech
+            </p>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: 'app',
+        data() {
+            return {
+                theHotels: []
+            };
+        },
+        components: {
+        },
+        created: function () {
+            axios.get('http://localhost:8080/hotels')
+                .then(response => this.theHotels = response.data)
+        }
+    }
+</script>
+
+<style>
+    #app {
+    }
+</style>
--- hotel-quarkus/src/components/HotelList.vue
+++ hotel-quarkus/src/components/HotelList.vue
@@ -1,13 +1,43 @@
 <template>
-    
+    <div class="pricing-tables pure-g">
+        <div class="pure-u-1">
+            <h3>Hotel list</h3>
+            <p>Discover our nice and superior list of hotels : </p>
+        </div>
+        <div class="pure-u-1 pure-u-md-1-3" v-for="hotel in hotels" :key="hotel.id">
+            <div class="pricing-table pricing-table-free">
+                <div class="pricing-table-header">
+                    <h2>{{ hotel.name }}</h2>
+
+                    <span class="pricing-table-price">
+                        {{ hotel.price }} <span>price in &euro; for one night</span>
+                    </span>
+
+
+                    <img v-bind:src="hotel.hotelImageURL" class="pure-u-1 pure-image">
+                </div>
+
+                <ul class="pricing-table-list">
+                    <li>{{ hotel.address }}</li>
+                    <li>{{ hotel.zip }} {{ hotel.city }}</li>
+                    <li>Valid for one standard room</li>
+                </ul>
+
+                <button class="button-choose pure-button">Choose</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "HotelList"
+        name: "HotelList",
+        props: {hotels: {type: Array, required: true}},
     }
 </script>
 
 <style scoped>
+    .hotel-image{
 
+    }
 </style>

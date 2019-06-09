cdfb4fe41e59f19876ecc343ae7de9dbf8e7d258
Nicolas Martignole <nicolas@touilleur-express.fr>
2019-06-09 19:29:10 +0200

    renamed

--- src/test/java/org/acme/hibernate/orm/panache/NativeFruitsEndpointIT.java
+++ src/test/java/org/acme/hibernate/orm/panache/NativeFruitsEndpointIT.java
@@ -3,7 +3,7 @@ package org.acme.hibernate.orm.panache;
 import io.quarkus.test.junit.SubstrateTest;
 
 @SubstrateTest
-public class NativeFruitsEndpointIT extends FruitsEndpointTest {
+public class NativeFruitsEndpointIT extends HotelsEndpointTest {
 
     // Runs the same tests as the parent class
 

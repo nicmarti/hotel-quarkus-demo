51ba51fad570db4d45fd152afb991aada4ad2c8a
Nicolas Martignole <nicolas@martignole.net>
2019-06-11 13:28:29 +0200

    CORS Filter : this is temporary and should be removed once Quarkus 0.17.0 is out

--- src/main/java/org/acme/hibernate/orm/panache/CORSFilter.java
+++ src/main/java/org/acme/hibernate/orm/panache/CORSFilter.java
@@ -5,7 +5,6 @@ import org.jboss.resteasy.spi.CorsHeaders;
 
 import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.container.ContainerRequestContext;
-import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Response;
@@ -25,20 +24,18 @@ import java.io.IOException;
 @Provider
 public class CORSFilter implements ContainerResponseFilter {
 
-    private String allowedOrigins="*";
-
-
     @Override
     public void filter(ContainerRequestContext requestContext,
                        ContainerResponseContext responseContext) throws IOException {
-        System.out.println("Filter CORS 1");
+        System.out.println("Filter CORS");
+
         String origin = requestContext.getHeaderString(CorsHeaders.ORIGIN);
         if (origin == null) {
             return;
         }
         if (requestContext.getMethod().equalsIgnoreCase("OPTIONS")) {
-            preflight(origin, requestContext);
-        }else {
+            preflight(origin, requestContext, responseContext);
+        } else {
             responseContext.getHeaders().add(
                     CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
             responseContext.getHeaders().add(
@@ -55,21 +52,19 @@ public class CORSFilter implements ContainerResponseFilter {
         }
     }
 
-    protected void preflight(String origin, ContainerRequestContext requestContext) throws IOException {
+    protected void preflight(String origin, ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         checkOrigin(requestContext, origin);
-
         System.out.println("Filter CORS preflight check");
-
-        Response.ResponseBuilder builder = Response.ok();
-        builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin);
-        builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS, HEAD");
-        builder.header(CorsHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "origin, content-type, accept, authorization");
-
-        requestContext.abortWith(builder.build());
+        responseContext.setStatus(200);
+        responseContext.getHeaders().add(CorsHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        responseContext.getHeaders().add(CorsHeaders.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, OPTIONS, DELETE, PUT, HEAD, PATCH");
+        responseContext.getHeaders().add(CorsHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "origin, content-type, accept, authorization");
+        responseContext.getHeaders().add(CorsHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        responseContext.getHeaders().add("content-type", "text/html; charset=utf=8");
     }
 
     protected void checkOrigin(ContainerRequestContext requestContext, String origin) {
+        String allowedOrigins = "*";
         if (!allowedOrigins.contains("*") && !allowedOrigins.contains(origin)) {
             System.out.println("Origin error cors.failure");
             requestContext.setProperty("cors.failure", true);
